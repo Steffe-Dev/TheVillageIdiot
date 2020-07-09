83d23b6339efa45de85a1864e69eec3bfd8176de
Steffe-Dev <52746596+Steffe-Dev@users.noreply.github.com>
2020-07-09 13:22:29 +0200

    trying to fix catch all

--- bot.py
+++ bot.py
@@ -79,7 +79,7 @@ async def on_message(message):
         urls = re.findall("https://i.imgur.com/.{13}[.]{1}[jpngif]{3}", message.content)
     
     if len(urls) == 0:
-        urls = re.findall("https://.*[.]{1}[jpngif]{3}", message.content)
+        urls = re.findall("https://i.*[.]{1}[jpngif]{3}", message.content)
 
     if message.channel.name == "memes-raw":
         if len(urls) > 0:
